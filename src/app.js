ddba276703abb2614ff9408a65719e1e922d6ca6
@vanshkansal25 <kansalv578@gmail.com>
2025-05-24 17:31:11 +0530

    add user registration functionality with multer and cloudinary integration

--- src/app.js
+++ src/app.js
@@ -22,4 +22,15 @@ app.use(express.static("public"))
 // this cookie-parser is used to server se user ke brower ke cookie access kr pau 
 // aur jrurat h toh crud operation perform kr pau 
 app.use(cookieParser()) 
+
+
+
+// routes
+
+import userRouter from "./routes/user.routes.js"
+
+
+app.use("/api/v1/users",userRouter)
+
+
 export {app}
